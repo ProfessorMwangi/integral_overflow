f56711e5c43002b9e1ca751520c45820a6da748d
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2026-02-08 20:33:14 +0300

    Demonstrates integer overflow handling
    Illustrates how Rust handles integer overflow by explicitly using `wrapping_add`. This prevents potential panics from unchecked arithmetic while showcasing safe overflow behavior.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-let x:i8 = 127;
-println!("{}",x + 1);
-
+    let x: i8 = 127;
+    // This overflows
+    // println!("{}",x + 1);
+    // Rust forces you to declare your intent....Therefore
+    let y = x.wrapping_add(1);
+    println!("Wrapping Add 1 to x is = {}", y);
 }
