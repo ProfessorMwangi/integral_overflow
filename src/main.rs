2945ee2a83fc8674f23915d47c528b797d82c366
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2026-02-08 21:45:36 +0300

    Adds saturating integer addition example
    Introduces an example for `saturating_add` to the main function, completing the demonstration of Rust's integer overflow handling methods. This provides a more comprehensive overview alongside `wrapping_add`, `checked_add`, and `overflowing_add`.
    
    Also refines the binary output format for `wrapping_add` for improved clarity.

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ fn main() {
     let x: i8 = 127;
     // This overflows
     // println!("{}",x + 1);
-    // Rust forces you to declare your intent....Therefore
+    // Rust forces you to declare your intent....Therefore it has this 4 Overflow API's
     let y = x.wrapping_add(1);
-    println!("Wrapping Add 1 to x ({1:08b}) is = {0}({0:08b})", y,x);
+    println!("Wrapping Add 1 to x ({1:08b}) is = {0}({0:b})", y,x);
 
     let y1 = x.checked_add(1);
     println!("Checked Add 1 to x is {:?}",y1);
 
     let (y2, overflowed)= x.overflowing_add(1);
-    println!("Overflowed Add 1 to x is {}.Which returns ({0},{1})", y2, overflowed)
+    println!("Overflowed Add 1 to x is {}.Which returns ({0},{1})", y2, overflowed);
+
+    let y3 = x.saturating_add(1);
+    println!("Saturating Add 1 to x is {}",y3)
 
 
 }
